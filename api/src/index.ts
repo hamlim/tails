8fafae63040e3489c632a3698819339e4478b374
Matt Hamlin <matthewjameshamlin@gmail.com>
2023-12-26 10:05:39 -0800

    Formatting, auth middleware

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,32 +1,50 @@
-import { Hono } from 'hono'
-import { cors } from 'hono/cors'
+import { type Context, Hono } from "hono";
+// import { deleteCookie, getCookie, getSignedCookie, setCookie, setSignedCookie } from "hono/cookie";
+import { cors } from "hono/cors";
 
-const app = new Hono()
+type Bindings = {
+  // MY_BUCKET: R2Bucket
+  TOKEN: string;
+};
 
-app.use('*', cors())
+const app = new Hono<{ Bindings: Bindings }>();
 
-app.get('/v1/login', (c) => {
+async function auth(c: Context, next) {
+  let token = c.req.header("auth-token");
+  console.log(c.env.TOKEN);
+  if (!token || token !== c.env.TOKEN) {
+    c.status(401);
+    return c.json({ error: "Unauthorized" });
+  }
+  await next();
+}
+
+app.use("*", cors());
+
+app.use("*", auth);
+
+app.get("/v1/login", (c) => {
   // login
-})
+});
 
-app.get('/v1/signup', (c) => {
+app.get("/v1/signup", (c) => {
   // signup
-})
+});
 
-app.get('/v1/recipes', (c) => {
+app.get("/v1/recipes", (c) => {
   // return recipes
 });
 
-app.get('/v1/recipe/:id', (c) => {
+app.get("/v1/recipe/:id", (c) => {
   // get specific recipe
-})
+});
 
-app.post('/v1/create-recipe', (c) => {
+app.post("/v1/create-recipe", (c) => {
   // create recipe
-})
+});
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-export default app
+export default app;
